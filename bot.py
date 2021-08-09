a8e224cf90801254c4f8f89b213e2df73be46461
Samank Gupta <samankgupta@gmail.com>
2021-08-09 12:08:02 +0530

    Deploying Bot to Heroku

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-from os import read
 import discord
+from discord.ext import commands
+import json
 
 
 def read_token():
@@ -10,15 +11,45 @@ def read_token():
 
 token = read_token()
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix="$", intents=intents)
+
+memFile = open('membershipids.txt', 'r')
+membershipids = memFile.readlines()
 
 
-@client.event
-async def on_message(message):
-    if str(message.channel).startswith("Direct Message"):
-        if message.content.find("$membership") == 0:
-            print(message.author)
-            print(message.content)
-            await message.channel.send("Please wait while we validate your Membership ID")
+@client.command()
+async def membership(ctx, *, id):
+    if(str(ctx.channel).startswith("Direct Message")):
+        flag = False
+        for member in membershipids:
+            if member.strip() == id:
+                flag = True
+                break
+        if flag:
+            with open("discordmembers.json", "r") as membersJsonFile:
+                membersJson = membersJsonFile.read()
+                if len(membersJson) == 0:
+                    tem = {}
+                else:
+                    tem = json.loads(membersJson)
+                if(tem.get(id)):
+                    await ctx.send(f'''Member with IEEE Membership ID {id} already exists.''')
+                else:
+                    tem[id] = str(ctx.message.author)
+                    jsonData = json.dumps(tem, indent=4)
+                    f = open("discordmembers.json", "w")
+                    f.write(jsonData)
+                    f.close()
+                    serverid = 870629789667569744
+                    server = client.get_guild(serverid)
+                    member = server.get_member(ctx.message.author.id)
+                    role = discord.utils.get(server.roles, name='member')
+                    await member.add_roles(role)
+                    await ctx.send("You have been assigned the member role. Go to roles-assignment channel on the discord server to select your Squad and Interests.")
+        else:
+            await ctx.send("IEEE Membership ID incorrect.")
 
 client.run(token)
